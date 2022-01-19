c6188f80b2862cd2fb10ea8f3aed0ad755c7776d
imkf-zhang <15638452918@163.com>
2022-01-19 22:24:35 +0800

    [study]在node中运行es6语法

---
+++ 3、node中体验es6模块化.js/main.js
@@ -0,0 +1,12 @@
+ const a = 88;
+ const b = 99;
+ const c = 100
+// 按需导出
+ export { 
+   c
+ }
+// 默认导出
+ export default {
+   a,
+   b
+ }
---
+++ 3、node中体验es6模块化.js/my.js
@@ -0,0 +1,7 @@
+import handle from './main'
+import { c } from './main'
+// 直接导入并执行
+import "./myOther"
+
+console.log(handle)  //{ a: 88, b: 99 }
+console.log(c)  // 100
