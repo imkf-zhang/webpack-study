af73955ad56395d4dfc6db6d3671e0bb4c6582a3
imkf-zhang <15638452918@163.com>
2022-01-20 22:49:56 +0800

    [study]打包的入口和出口

--- 4、webpack/2/webpack.config.js
+++ 4、webpack/2/webpack.config.js
@@ -1,4 +1,10 @@
+const path = require('path');
 module.exports = {
   // 编译模式，mode用来指定构建模式
   mode: 'development', // development production 
+  entry: path.join(__dirname,'./src/index.js'), //入口
+  output: {  //出口，输出
+    path: path.join(__dirname,'./dist'),
+    filename: 'bundle.js' //输出文件名称
+  }
 }
---
+++ 4、webpack/3、webpack默认打包的入口和出口.md
@@ -0,0 +1,7 @@
+webpack 4.x中默认规定
+
+打包入口文件为 src -> index.js
+
+打包的输出文件为 dist -> main.js
+
+想自己手动配置wenpack打包的入口和出口：
